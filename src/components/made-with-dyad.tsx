933ed098a27fe93e0a4d23a2595711278604a890
[dyad] <meloleao@pm.me>
2025-11-25 10:34:27 -0300

    [dyad] Removendo texto e link do componente MadeWithDyad - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -1,12 +1,3 @@
 export const MadeWithDyad = () => {
-  return (
-    <a
-      href="https://www.dyad.sh/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="p-4 text-center text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-    >
-      Made with Dyad
-    </a>
-  );
+  return null;
 };
